2fe6716acf6640760c662d72325f3ff87a24ae1b
Samanyu Das <106096565+samanyudas@users.noreply.github.com>
2026-01-01 23:30:15 +0400

    Save data and generate HTML for payment summary &Make payment summary interactive

--- data/products.js
+++ data/products.js
@@ -1,3 +1,15 @@
+export function getProduct(productId){
+  let matchingProduct;
+
+  products.forEach((product) => {
+      if (product.id === productId){
+          matchingProduct = product;
+      }
+  })
+
+  return matchingProduct;
+}
+
 // To access products in 'amazon.js' file
 export const products = [
   {
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,9 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {getProduct} from '../../data/products.js';
 import formatCurrency from '../../scripts/utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -11,21 +12,10 @@ export function renderOrderSummary(){
   cart.forEach((cartItem) => {
       const productId = cartItem.productId;
       
-      let matchingProduct;
-
-      products.forEach((product) => {
-          if (product.id === productId){
-              matchingProduct = product;
-          }
-      })
+      const matchingProduct = getProduct(productId);
 
       const deliveryOptionId = cartItem.deliveryOptionId;
-      let deliveryOption;
-      deliveryOptions.forEach((option) => {
-          if (option.id === deliveryOptionId){
-              deliveryOption = option;
-          }
-      });
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
 
       const today  = dayjs();
       const deliveryDate = today.add(
@@ -135,6 +125,8 @@ export function renderOrderSummary(){
               );
 
               container.remove();
+
+              renderPaymentSummary();
           });
       });
 
@@ -232,6 +224,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 
