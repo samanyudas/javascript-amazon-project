0a4869834ffff0178cd7c7ac4edf15fdbacc2a57
Samanyu Das <106096565+samanyudas@users.noreply.github.com>
2026-01-01 17:44:58 +0400

    Save data and generate HTML for delivery options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import {cart, addToCart, calculateCartQuantity} from "../data/cart.js";
 import {products} from "../data/products.js";
-import {formatCurrency} from './utils/money.js';
+import formatCurrency from './utils/money.js';
 
 let productsHTML = '';
 
