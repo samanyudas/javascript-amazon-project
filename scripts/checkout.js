a980c5e4afddfd944dedc676e8cf2de23e48a912
Samanyu Das <106096565+samanyudas@users.noreply.github.com>
2026-01-03 12:00:03 +0400

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 
 renderCheckoutHeader();
